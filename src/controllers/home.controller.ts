01a6a7b25b33131c220fd1d687fa4ef276844d03
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2024-04-08 10:30:52 +0200

    suppression home route

--- src/controllers/home.controller.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express";
-
-export function welcome(req: Request, res: Response): Response {
-  return res.json({ message: "Welcome to bezkoder application." });
-}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,9 @@ import { Application } from "express";
 import candidatRoutes from "./candidat.routes";
 import recruteurRoutes from "./recruteur.routes";
 import entretienRoutes from './entretien.routes';
-import homeRoutes from "./home.routes";
 
 export default class Routes {
   constructor(app: Application) {
-    app.use("/api", homeRoutes);
     app.use("/api/candidat", candidatRoutes);
     app.use("/api/recruteur", recruteurRoutes);
     app.use("/api/entretien", entretienRoutes);
