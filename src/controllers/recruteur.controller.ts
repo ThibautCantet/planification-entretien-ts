39ef946a3eca64fc805cb734704be9f08a19c081
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2024-04-08 17:35:35 +0200

    refacto: move code in recruteur service for save recruteur

--- src/controllers/recruteur.controller.ts
+++ src/controllers/recruteur.controller.ts
@@ -4,24 +4,8 @@ import recruteurService from '../services/recruteur.service';
 
 export default class RecruteurController {
   async create(req: Request, res: Response) {
-
-    let isEmailValid:boolean;
-
-    const regexp: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-
-    isEmailValid = regexp.test(req.body.email);
-
-    if (!req.body.langage || !req.body.xp || req.body.xp < 0 || !req.body.email || !isEmailValid) {
-      res.status(400).send({
-        message: "Content can not be empty!"
-      });
-      return;
-    }
-
     try {
-      const recruteur: Recruteur = req.body;
-
-      const savedRecruteur = await recruteurService.save(recruteur);
+      const savedRecruteur = await recruteurService.save(req, res);
 
       res.status(201).send(savedRecruteur);
     } catch (err) {
--- src/services/recruteur.service.ts
+++ src/services/recruteur.service.ts
@@ -1,10 +1,28 @@
 import Recruteur from '../models/recruteur.model';
 import recruteurRepository from '../repositories/recruteur.repository';
+import { Request, Response } from 'express';
 
 class RecruteurService {
 
-    async save(recruteur: Recruteur): Promise<Recruteur> {
-        return await recruteurRepository.save(recruteur);
+    async save(req: Request, res: Response) {
+        let isEmailValid: boolean;
+
+        const regexp: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+        isEmailValid = regexp.test(req.body.email);
+
+        if (!req.body.langage || !req.body.xp || req.body.xp < 0 || !req.body.email || !isEmailValid) {
+            res.status(400).send({
+                message: 'Content can not be empty!'
+            });
+            return;
+        }
+
+        const recruteur: Recruteur = req.body;
+
+        const savedRecruteur = await recruteurRepository.save(recruteur);
+
+        res.status(201).send(savedRecruteur);
     }
 
     async retrieveAll(searchParams: { email?: string }): Promise<Recruteur[]> {
